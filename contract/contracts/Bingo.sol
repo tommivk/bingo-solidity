60354a273e7f0e3d85a6c2e1c32b031bc033061c
tommivk <tommikokk@gmail.com>
2022-11-26 21:39:15 +0200

    add constructor

--- contract/contracts/Bingo.sol
+++ contract/contracts/Bingo.sol
@@ -4,5 +4,11 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Bingo {
+    uint public ticketCost;
+    address public host;
 
+    constructor(uint _ticketCost) payable {
+        ticketCost = _ticketCost;
+        host = msg.sender;
+    }
 }
