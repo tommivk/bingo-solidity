22f2b2ebd11417f9797605cf5181015ad0579868
tommivk <tommikokk@gmail.com>
2022-11-26 23:47:35 +0200

    add generateCard function

--- contract/contracts/Bingo.sol
+++ contract/contracts/Bingo.sol
@@ -12,6 +12,35 @@ contract Bingo {
         host = msg.sender;
     }
 
+    function generateCard() private view returns (uint8[25] memory) {
+        uint8[] memory nums = getNumbers();
+
+        // Randomize the array of numbers
+        // Note This is pseudo-random
+        for (uint i = 0; i < nums.length - 1; i++) {
+            uint j = (uint(
+                keccak256(
+                    abi.encodePacked(
+                        i,
+                        block.difficulty,
+                        block.timestamp,
+                        msg.sender
+                    )
+                )
+            ) % nums.length);
+            uint8 temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        // Choose the first 25 values as the bingo card
+        uint8[25] memory card;
+        for (uint i = 0; i < 25; i++) {
+            card[i] = nums[i];
+        }
+        return card;
+    }
+
     function getNumbers() public pure returns (uint8[] memory) {
         uint8[] memory numbers = new uint8[](75);
         for (uint i = 0; i < 75; i++) {
