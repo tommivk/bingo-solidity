5324686166d3453dd6adcfe62d33cd82ae8b61a9
tommivk <tommikokk@gmail.com>
2022-11-26 23:46:44 +0200

    add getNumbers function

--- contract/contracts/Bingo.sol
+++ contract/contracts/Bingo.sol
@@ -11,4 +11,12 @@ contract Bingo {
         ticketCost = _ticketCost;
         host = msg.sender;
     }
+
+    function getNumbers() public pure returns (uint8[] memory) {
+        uint8[] memory numbers = new uint8[](75);
+        for (uint i = 0; i < 75; i++) {
+            numbers[i] = uint8(i + 1);
+        }
+        return numbers;
+    }
 }
