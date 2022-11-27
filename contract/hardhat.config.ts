cd895bdd3e588319645e635ba8598513b05b3b40
tommivk <tommikokk@gmail.com>
2022-11-27 23:46:32 +0200

    add smock

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,7 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          outputSelection: {
+            "*": {
+              "*": ["storageLayout"],
+            },
+          },
+        },
+      },
+    ],
+  },
 };
 
 export default config;
