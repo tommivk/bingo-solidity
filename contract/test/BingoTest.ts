1aedc1b81ff0c46afd4a164c6ba365483e2b236b
tommivk <tommikokk@gmail.com>
2022-11-28 00:21:24 +0200

    add tests

--- contract/test/BingoTest.ts
+++ contract/test/BingoTest.ts
@@ -2,8 +2,11 @@ import { expect } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { smock, MockContract } from "@defi-wonderland/smock";
+import { Bingo, Bingo__factory } from "../typechain-types";
 
 const ticketCost = 200;
+const maxPlayers = 3;
 
 let accountA: SignerWithAddress;
 let accountB: SignerWithAddress;
@@ -13,8 +16,10 @@ describe("Bingo", function () {
   let bingo: Contract;
 
   beforeEach(async () => {
-    const Bingo = await ethers.getContractFactory("Bingo");
-    bingo = await Bingo.deploy(ticketCost, { value: ticketCost });
+    const contract = await ethers.getContractFactory("Bingo");
+    bingo = await contract.deploy(ticketCost, maxPlayers, {
+      value: ticketCost,
+    });
 
     [accountA, accountB, accountC] = await ethers.getSigners();
   });
@@ -44,5 +49,94 @@ describe("Bingo", function () {
       }
     });
     expect(set.size).to.equal(25);
+  });
+});
+
+describe("checkBingo tests", async () => {
+  let bingo: MockContract<Bingo>;
+
+  beforeEach(async () => {
+    const Contract = await smock.mock<Bingo__factory>("Bingo");
+    bingo = await Contract.deploy(ticketCost, maxPlayers, {
+      value: ticketCost,
+    });
+
+    [accountA, accountB, accountC] = await ethers.getSigners();
+
+    // Set numbers 1-5 as drawn
+    await bingo.setVariables({
+      numbersDrawn: {
+        1: true,
+        2: true,
+        3: true,
+        4: true,
+        5: true,
+      },
+    });
+  });
+
+  it("checkBingo should return false with bingo card filled with zeroes", async () => {
+    const card = new Array(25).fill(0);
+    const result = await bingo.checkBingo(card);
+    expect(result).to.equal(false);
+  });
+
+  it("checkBingo should return true when there is a horizontal bingo", async () => {
+    const numbers = [1, 2, 3, 4, 5];
+
+    let card = [...numbers, ...Array(20).fill(0)];
+    expect(await bingo.checkBingo(card)).to.equal(true);
+
+    card = [...Array(5).fill(0), ...numbers, ...Array(15).fill(0)];
+    expect(await bingo.checkBingo(card)).to.equal(true);
+
+    card = [...Array(10).fill(0), ...numbers, ...Array(10).fill(0)];
+    expect(await bingo.checkBingo(card)).to.equal(true);
+
+    card = [...Array(15).fill(0), ...numbers, ...Array(5).fill(0)];
+    expect(await bingo.checkBingo(card)).to.equal(true);
+
+    card = [...Array(20).fill(0), ...numbers];
+    expect(await bingo.checkBingo(card)).to.equal(true);
+  });
+
+  it("checkBingo should return true when there is a diagonal bingo", async () => {
+    let card = new Array(25).fill(0);
+    card[0] = 1;
+    card[6] = 2;
+    card[18] = 4;
+    card[24] = 5;
+    expect(await bingo.checkBingo(card)).to.equal(true);
+
+    card = new Array(25).fill(0);
+    card[4] = 1;
+    card[8] = 2;
+    card[16] = 4;
+    card[20] = 5;
+    expect(await bingo.checkBingo(card)).to.equal(true);
+  });
+
+  it("checkBingo should return true when there is a vertical bingo", async () => {
+    for (let i = 0; i < 5; i++) {
+      let card = new Array(25).fill(0);
+
+      for (let j = 0; j < 5; j++) {
+        let columnIndex = 5 * j + i;
+        card[columnIndex] = j + 1;
+      }
+
+      const res = await bingo.checkBingo(card);
+      expect(res).to.equal(true);
+    }
+  });
+
+  it("checkBingo should return false with random non-winning bingo cards", async () => {
+    for (let i = 0; i < 50; i++) {
+      let card = [...Array(76).keys()];
+      card.splice(0, 3);
+      card.sort(() => 0.5 - Math.random());
+      card = card.slice(0, 25);
+      expect(await bingo.checkBingo(card)).to.equal(false);
+    }
   });
 });
