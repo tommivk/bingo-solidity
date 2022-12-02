d203d7e9e15fa275b83182584d00142e7180c904
tommivk <tommikokk@gmail.com>
2022-12-02 23:20:41 +0200

    add tests

---
+++ contract/test/BingoFactoryTest.ts
@@ -0,0 +1,66 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Contract } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+let bingoFactory: Contract;
+let contractAddress: string;
+let accountA: SignerWithAddress;
+let accountB: SignerWithAddress;
+
+describe("BingoFactory tests", () => {
+  beforeEach(async () => {
+    const contract = await ethers.getContractFactory("BingoFactory");
+    [accountA, accountB] = await ethers.getSigners();
+    bingoFactory = await contract.deploy(accountA.address);
+    contractAddress = bingoFactory.address;
+  });
+
+  it("Owner of the contract should be set correctly", async () => {
+    const owner = await bingoFactory.owner();
+    expect(owner).to.equal(accountA.address);
+  });
+
+  it("Only owner should be able to change owner", async () => {
+    await expect(
+      bingoFactory.connect(accountB).changeOwner(accountB.address)
+    ).to.be.revertedWith("Only contract owner can call this function");
+    await bingoFactory.changeOwner(accountB.address);
+    expect(await bingoFactory.owner()).to.equal(accountB.address);
+  });
+
+  it("Sending ether to the contract should succeed", async () => {
+    const oneEther = ethers.utils.parseEther("1");
+
+    await accountB.sendTransaction({
+      to: contractAddress,
+      value: oneEther,
+    });
+    const balance = await ethers.provider.getBalance(contractAddress);
+    expect(balance).to.equal(oneEther);
+  });
+
+  it("Owner should be able to withdraw contract funds", async () => {
+    const oneEther = ethers.utils.parseEther("1");
+
+    await accountB.sendTransaction({
+      to: contractAddress,
+      value: oneEther,
+    });
+    const balanceBefore = await ethers.provider.getBalance(accountA.address);
+    const tx = await bingoFactory.withdraw();
+    const balanceAfter = await ethers.provider.getBalance(accountA.address);
+
+    const receipt = await tx.wait();
+    const gasUsed =
+      BigInt(receipt.cumulativeGasUsed) * BigInt(receipt.effectiveGasPrice);
+
+    expect(balanceAfter.add(gasUsed).sub(balanceBefore)).to.equal(oneEther);
+  });
+
+  it("Only owner should be able to withdraw", async () => {
+    await expect(bingoFactory.connect(accountB).withdraw()).to.be.rejectedWith(
+      "Only contract owner can call this function"
+    );
+  });
+});
