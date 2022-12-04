a33f057014373b49747ce586f8609bc5202f5995
tommivk <tommikokk@gmail.com>
2022-12-04 21:06:27 +0200

    add wagmi config

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { chain, createClient, WagmiConfig, configureChains } from "wagmi";
+import { Web3Modal } from "@web3modal/react";
+import { EthereumClient } from "@web3modal/ethereum";
+import { publicProvider } from "wagmi/providers/public";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+const PROJECT_ID = process.env.WALLETCONNECT_PROJECT_ID ?? "";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const chains = [chain.hardhat];
+  const { provider } = configureChains(chains, [publicProvider()]);
+
+  const client = createClient({
+    autoConnect: true,
+    connectors: [
+      new InjectedConnector({ chains }),
+      new WalletConnectConnector({
+        chains,
+        options: {
+          qrcode: false,
+        },
+      }),
+    ],
+    provider,
+  });
+
+  const ethereumClient = new EthereumClient(client, chains);
+
+  return (
+    <>
+      <WagmiConfig client={client}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+      <Web3Modal projectId={PROJECT_ID} ethereumClient={ethereumClient} />
+    </>
+  );
 }
