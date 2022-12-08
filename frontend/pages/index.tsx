a8d9f8cd98cf9d572c9fc2b0f2bd36d9bab5bfe3
tommivk <tommikokk@gmail.com>
2022-12-08 17:52:56 +0200

    add Home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,73 @@
+import Head from "next/head";
+import Link from "next/link";
+import { Web3Button } from "@web3modal/react";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useContractRead,
+  useAccount,
+  useContractEvent,
+} from "wagmi";
+import { ethers } from "ethers";
+import BingoFactory from "../abi/BingoFactory.json";
+
+const BINGO_FACTORY_ADDRESS = process.env.NEXT_PUBLIC_BINGO_FACTORY_ADDRESS;
+
+const ticketCost = 200;
+const maxPlayers = 5;
+
 export default function Home() {
+  const { address } = useAccount();
+  const contractData = {
+    address: BINGO_FACTORY_ADDRESS,
+    abi: BingoFactory.abi,
+  };
+
+  const { config, error } = usePrepareContractWrite({
+    ...contractData,
+    functionName: "createRoom",
+    overrides: {
+      value: ticketCost,
+    },
+    args: [ethers.BigNumber.from(ticketCost), maxPlayers],
+  });
+  const { write: createRoom } = useContractWrite(config);
+
+  const { data, refetch }: { data?: Array<string>; refetch: any } =
+    useContractRead({
+      ...contractData,
+      functionName: "getContracts",
+    });
+
+  useContractEvent({
+    ...contractData,
+    eventName: "NewRoomCreated",
+    listener() {
+      refetch();
+    },
+  });
+
   return (
     <div>
-      <h1>Hello</h1>
+      <Head>
+        <title>Bingo</title>
+        <meta name="description" content="Bingo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Web3Button />
+      connected address: {address}
+      <button onClick={() => createRoom?.()}>Create new room</button>
+      <h1>Rooms</h1>
+      <ul>
+        {data
+          ?.slice(0)
+          .reverse()
+          .map((d: string) => (
+            <li key={d}>
+              <Link href={`/games/${d}`}>{d}</Link>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
