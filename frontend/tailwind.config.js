76628a1b56563f2b3c757f38e92b35c4bd42c115
tommivk <tommikokk@gmail.com>
2022-12-22 19:32:28 +0200

    add color variable

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkSecondary: "#2c2d3382",
+      }
+    },
   },
   plugins: [],
 }
