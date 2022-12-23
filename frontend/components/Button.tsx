d473861a6e47338ebf00dd62bd34be869f0cb39e
tommivk <tommikokk@gmail.com>
2022-12-23 16:08:54 +0200

    prevent loading spinner from changing button size

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -9,9 +9,16 @@ const Button = ({ children, loading, ...props }: Props) => {
   return (
     <button
       {...props}
-      className={`${props.className} rounded-lg bg-slate-700 disabled:bg-slate-700 disabled:pointer-events-none hover:bg-slate-600 px-5 py-2 text-slate-200`}
+      className={`${props.className} relative rounded-lg bg-slate-700 disabled:bg-slate-700 disabled:pointer-events-none hover:bg-slate-600 px-5 py-2 text-slate-200`}
     >
-      {loading ? <Spinner size={20} /> : children}
+      <span className={`${loading ? "invisible" : ""}`}>
+        {children}
+        {loading && (
+          <span className="visible absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <Spinner size={20} />
+          </span>
+        )}
+      </span>
     </button>
   );
 };
