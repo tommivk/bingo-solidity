a7ddfca6e566436dd9b4c807630f3aa2e4c5c77b
tommivk <tommikokk@gmail.com>
2023-01-08 16:32:43 +0200

    send vrf variables to constructor

--- contract/contracts/BingoFactory.sol
+++ contract/contracts/BingoFactory.sol
@@ -8,12 +8,10 @@ import "./LinkTokenInterface.sol";
 contract BingoFactory {
     VRFCoordinatorV2Interface COORDINATOR;
     LinkTokenInterface LINKTOKEN;
-    // ** Mumbai network **
-    address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
-    address linkTokenContract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-    // The gas lane to use, which specifies the maximum gas price to bump to
-    bytes32 keyHash =
-        0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+
+    address vrfCoordinator;
+    address linkTokenContract;
+    bytes32 keyHash;
     uint64 public vrfSubscriptionId;
 
     address public owner;
@@ -27,8 +25,16 @@ contract BingoFactory {
     );
     event OwnerChanged(address indexed _newOwner);
 
-    constructor(address _owner) {
+    constructor(
+        address _owner,
+        address _vrfCoordinator,
+        address _linkTokenContract,
+        bytes32 _keyHash
+    ) {
         owner = _owner;
+        vrfCoordinator = _vrfCoordinator;
+        linkTokenContract = _linkTokenContract;
+        keyHash = _keyHash;
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
         LINKTOKEN = LinkTokenInterface(linkTokenContract);
         vrfSubscriptionId = COORDINATOR.createSubscription();
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -3,10 +3,23 @@ import { ethers } from "hardhat";
 async function main() {
   const [deployer] = await ethers.getSigners();
 
+  // ** Mumbai network **
+  const vrfCoordinator = "0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed";
+  const linkTokenContract = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
+  // The gas lane to use, which specifies the maximum gas price to bump to
+  const keyHash =
+    "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f";
+
   const BingoFactory = await ethers.getContractFactory("BingoFactory");
-  const bingoFactory = await BingoFactory.deploy(deployer.address, {
-    gasLimit: 9000000,
-  });
+  const bingoFactory = await BingoFactory.deploy(
+    deployer.address,
+    vrfCoordinator,
+    linkTokenContract,
+    keyHash,
+    {
+      gasLimit: 9000000,
+    }
+  );
 
   await bingoFactory.deployed();
 
