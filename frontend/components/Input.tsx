bb0e2306876f1e6f4cf24d27828630dd27e4f423
tommivk <tommikokk@gmail.com>
2023-01-15 00:44:49 +0200

    define unallowed characters to number input

--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,11 +1,25 @@
 const Input = ({
   className,
+  allowDecimals = true,
+  allowNegative = true,
   ...props
-}: React.InputHTMLAttributes<HTMLInputElement>) => {
+}: React.InputHTMLAttributes<HTMLInputElement> & {
+  allowDecimals?: boolean;
+  allowNegative?: boolean;
+}) => {
+  const unallowedChars = ["e", "E", "+"];
+  if (!allowDecimals) unallowedChars.push(".");
+  if (!allowNegative) unallowedChars.push("-");
+
   return (
     <input
       {...props}
       className={`${className} py-1 px-2 rounded-sm text-slate-900`}
+      onKeyDown={(event) =>
+        props.type === "number" &&
+        unallowedChars.includes(event.key) &&
+        event.preventDefault()
+      }
     ></input>
   );
 };
