c4f981af7cc454ea6f7c702d71c9b6047c3149c1
tommivk <tommikokk@gmail.com>
2023-01-22 00:10:04 +0200

    fix error messages positioning

--- frontend/components/WrongNetworkError.tsx
+++ frontend/components/WrongNetworkError.tsx
@@ -1,6 +1,6 @@
 const WrongNetworkError = () => {
   return (
-    <div className="absolute w-screen h-screen bg-black opacity-80 z-40 flex justify-center items-center">
+    <div className="fixed top-0 w-screen h-screen bg-black opacity-80 z-40 flex justify-center items-center">
       <h1 className="text-slate-200 text-2xl z-50 opacity-100 p-10">
         Please switch To Polygon Mumbai Network
       </h1>
