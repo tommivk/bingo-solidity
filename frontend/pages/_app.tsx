328ee8135b2c74d32eb014ca77bb1e26278986c9
tommivk <tommikokk@gmail.com>
2023-07-31 22:43:43 +0300

    bump wagmi version and use alchemy as provider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,35 +1,41 @@
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
-import { chain, createClient, WagmiConfig, configureChains } from "wagmi";
+import { WagmiConfig, configureChains, createClient } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
 import { Web3Modal } from "@web3modal/react";
 import { EthereumClient } from "@web3modal/ethereum";
-import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { ToastContainer } from "react-toastify";
 import NextNProgress from "nextjs-progressbar";
 
 const PROJECT_ID = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "";
+const ACHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY ?? "";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const chains = [chain.polygonMumbai];
-  const { provider } = configureChains(chains, [publicProvider()], {
-    pollingInterval: 1000,
+  const chains = [polygonMumbai];
+  const { provider } = configureChains(
+    chains,
+    [alchemyProvider({ apiKey: ACHEMY_API_KEY })],
+    {
+      pollingInterval: 5000,
+    }
+  );
+
+  const connector = new WalletConnectConnector({
+    chains,
+    options: {
+      projectId: PROJECT_ID,
+      showQrModal: false,
+    },
   });
 
   const client = createClient({
     autoConnect: true,
-    connectors: [
-      new InjectedConnector({ chains }),
-      new WalletConnectConnector({
-        chains,
-        options: {
-          qrcode: false,
-        },
-      }),
-    ],
     provider,
+    connectors: [new InjectedConnector({ chains }), connector],
   });
 
   const ethereumClient = new EthereumClient(client, chains);
